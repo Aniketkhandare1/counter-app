d0cd9d712cc30dbe69956f255eb223024b564c6a
Aniket <aniketkhandare250@gmail.com>
2023-07-03 20:22:24 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import './App.css'
     const [count,setCount] =useState(0)
   return (
     <div className="box" >
-        <div >
+        <div className="heading" >
             <h1>COUNTER</h1>
-        </div>
-        <div>
-            <div className="num"><h2> {count}</h2></div>
-            
-            <div className="btn">
-            <button onClick={()=>setCount(count + 1)}>＋</button>
-            <button onClick={()=>setCount(0)}>RESET</button>
-            <button onClick={()=>(count< 1 ? "":setCount(count - 1))}>–</button>
-        </div>
+        </div> 
+        <div className="circle">
+            <h2> {count}</h2>
+             </div>
+             
+             <div>
+             <div className="btn">
+            <button  className="btn-one" onClick={()=>setCount(count + 1)}>＋</button>
+            <button  className="btn-two" onClick={()=>setCount(0)}>RESET</button>
+            <button  className="btn-three" onClick={()=>(count< 1 ? "":setCount(count - 1))}>–</button>
+             </div>
         </div>
 
     </div>
